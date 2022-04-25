ad6df97fa4851de57f8df788b70b913f21310c2d
NguyenTrung170801 <trungnguyenhoang936@gmail.com>
2022-04-25 18:22:22 +0700

    code

--- src/Buoi5/bai4.java
+++ src/Buoi5/bai4.java
@@ -1,9 +1,24 @@
 package Buoi5;
 
+
 public class bai4 {
     public static void main(String[] args) {
-        for (int i = 1; i < args.length; i++) {
-            System.out.println();
+        System.out.println("chuoi cu: " + "restart");
+        System.out.print("chuoi moi: ");
+        String str = "restart";
+        String str2 = "";
+        char a = str.charAt(0);
+        for (int i = 1; i < str.length(); i++) {
+            if (a == str.charAt(i)) {
+                str2 += '$';
+            } else str2 += str.charAt(i);
         }
+        System.out.println(a + str2);
     }
+
 }
+
+
+
+
+
--- src/buoi6/stringbuilder.java
+++ src/buoi6/stringbuilder.java
@@ -2,9 +2,6 @@ package buoi6;
 
 public class stringbuilder {
     public static void main(String[] args) {
-        char [] array = {'H', 'e', 'l', 'l','o'};
-        StringBuilder strb = new StringBuilder();
-        strb.append();
-        System.out.println(strb);
+
     }
 }
