eb84e3428b119ad1b845c3bb6937059523a93cd2
NguyenTrung170801 <trungnguyenhoang936@gmail.com>
2022-04-25 13:41:26 +0700

    code

--- src/Buoi5/bai2.java
+++ src/Buoi5/bai2.java
@@ -1,18 +1,28 @@
 package Buoi5;
-
 import java.util.Scanner;
 
 public class bai2 {
-    public static void main(String[] args) {
-        Scanner nhap = new Scanner(System.in);
-        int count = 0;
-        String str;
-        System.out.println("nhap");
-        str = nhap.nextLine();
-        for (int i = 0; i < str.length(); i++) {
-            for (int j = i+1; j <str.length()-1; j++) {
-            if(str)
+    static void DemKyTu (String str){
+            int[] dem = new int[256];
+            for (int i = 0; i < str.length(); i++) {
+                dem[str.charAt(i)]++;
             }
+
+            for (int i = 0; i < dem.length; i++) {
+                if (dem[i] != 0) {
+                    System.out.println("Ky tu " + (char) i + " xuat hien " + dem[i] + " lan ");
+                }
+
+            }
+
         }
+
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap chuoi: ");
+        String str = sc.nextLine();
+        System.out.println("Chuoi la: "+ str);
+        DemKyTu(str);
     }
 }
